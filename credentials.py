ce607d2526f72ce297d52738f95043ff561dc3b9
Siwin Lo <siwin.lo@gmail.com>
2020-02-07 20:35:06 -0500

    ~

--- credentials.py
+++
@@ -1,6 +0,0 @@
-consumer_key = "eVHUFVpw5qLumCjasBgLCZOlM"
-consumer_secret = "pWdPHcunkFR602qTocwcvMtx9BpSp1OERpha8jPEzvMG4mLMnw"
-
-access_token = "1225510617324322816-cdPeuspXi7G9LXZa15QDOhJWm2vxIQ"
-access_token_secret = "yhNPZuXUoFB14lNQ3fWZsz7AlbdJhU6lNqnJD2v0nB9aK"
-
